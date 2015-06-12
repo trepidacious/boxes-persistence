6e09aecf1810b32b8823d4d5ecce49fb18e46268
trepidacious <trepidacious@gmail.com>
2015-06-12 15:53:59 +0100

    Add doc on lazyFormat

--- src/main/scala/org/rebeam/boxes/persistence/BasicFormats.scala
+++ src/main/scala/org/rebeam/boxes/persistence/BasicFormats.scala
@@ -30,6 +30,14 @@ object BasicFormats {
 
   /**
    * Lazy format for nested/recursive case classes/nodes
+   * To use, wrap the normal Format for the recursive data, add a Format[T] annotation, and assign to a lazy val, e.g.
+   *
+   * case class Nested(i: Int, n: Option[Nested])
+   * implicit lazy val nestedFormat: Format[Nested] = lazyFormat(productFormat2(Nested.apply)("i", "n"))
+   *
+   * This breaks the cycle that would otherwise result in a compile time error or runtime stack overflow.
+   *
+   * @param format A non-lazy format to which to lazily delegate
    */
   def lazyFormat[T](format: => Format[T]) = new Format[T] {
     lazy val delegate = format
